bc67c72fcebd24572a78180a877a8cb4b8cafd2b
Monika Mandal <monika711205@gmail.com>
2024-06-26 10:04:33 +0530

    Commit-1

---
+++ src/main/java/com/devops/__jenkins_docker/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.devops.__jenkins_docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+
+        return "Hello";
+    }
+}
